3c0b0ca42ba534310bc578005e8ead603de47919
Enzo Mathe <enzo.mathe@gitguardian.com>
2021-07-06 14:43:30 +0200

    add secret

--- secret_leak.py
+++ secret_leak.py
@@ -7,6 +7,9 @@ touch index.html
 This will create an "index.html" file in your currently active directory.
 
 slack_key = xoxb-355342213163-FGqddMF3t56v3N3Oq2i22vs889203
+slack_key = xoxb-355342213163-FGqddMF3t56v3N3Oq2i22vs889203slack_key = xoxb-355342213163-FGqddMF3t56v3N3Oq2i22vs889203
+
+slack_key = xoxb-355342213163-FGqddMF3t56v3N3Oq2i22vs889203
 
 Step 3. Create Files with Touch
 Creating a file with Terminal is super easy. All you have to do is type "touch" followed by the name of the file that you wish to create.
